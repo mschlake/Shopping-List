bda540f96622bd15747eb3bbf4804200b9c3be46
Matthew Schlake <64107938+mschlake@users.noreply.github.com>
2020-06-13 20:03:02 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+// function to "delete" items from the list
+$(function(){
+  $('.shopping-item-delete').click(function(event){
+    $(this).closest('li').remove();
+  });
+});
+
+// function to "check" the item off the list (line through text) 
+$(function(){
+  $('.shopping-item-toggle').click
+  (function(event){
+    $(this).find('span').toggleClass('shopping-item__checked');
+  });
+});
+
+// function to add an item/buttons to the list when submit or press enter
+$(function(){
+  $('#js-shopping-list-form').submit(function(event){
+    event.preventDefault();
+    var newItem = $('#shopping-list-entry').val();
+  $('.shopping-list').append('<li>' + '<span class="shopping-item">' + newItem +'</span>' + '<div class="shopping-item-controls">' + '<button class="shopping-item-toggle">' + '<span class="button-label">check</span>' + '</button> ' + '<button class="shopping-item-delete">' + '<span class="button-label">delete</span>' + '</button>' + '</li>');
+ });
+});
